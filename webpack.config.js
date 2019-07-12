b756ea50098af74f4fed100aeae8dfbf5cdcfa12
James Rishe <jlrishe@users.noreply.github.com>
2019-07-12 16:30:03 +0900

    Webpack and Babel changes to make IE9+ compatible

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const UnminifiedWebpackPlugin = require('unminified-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-module.exports = {
+const htmldiff = {
     entry: {
         htmldiff: ['./src/Diff.js'],
     },
@@ -13,7 +13,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/dist/',
         library: 'HtmlDiff',
-        libraryTarget: 'commonjs2'
+        libraryTarget: 'umd'
     },
 
     module: {
@@ -35,3 +35,38 @@ module.exports = {
         minimize: true
     }
 };
+
+const polyfill = {
+    entry: {
+        'babel-polyfill': ['@babel/polyfill'],
+    },
+
+    output: {
+        filename: 'babel-polyfill.min.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/',
+        library: 'BabelPolyfill',
+        libraryTarget: 'umd'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new UnminifiedWebpackPlugin()
+    ],
+
+    optimization: {
+        minimize: true
+    }
+};
+
+module.exports = [ htmldiff, polyfill ];
